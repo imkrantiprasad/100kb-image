5f091bba76ea7f9dc730f88ed9ae2f632ae65dee
imkrantiprasad <krantiprasad12@gmail.com>
2025-02-09 18:27:05 +0530

    optimize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-// export const metadata: Metadata = {
-//   title: "Online Image Compressor",
-//   description:
-//     "100kb-image is the ultimate image optimizer to compress your images in JPEG, GIF and PNG formats to the minimum possible size.",
-//   openGraph: {
-//     title: "Online Image Compressor",
-//     description:
-//       "100kb-image is the ultimate image optimizer to compress your images in JPEG, GIF and PNG formats to the minimum possible size.",
-//     images: "/assets/hero.svg",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Online Image Compressor",
+  description:
+    "100kb-image is the ultimate image optimizer to compress your images in JPEG, GIF and PNG formats to the minimum possible size.",
+  openGraph: {
+    title: "Online Image Compressor",
+    description:
+      "100kb-image is the ultimate image optimizer to compress your images in JPEG, GIF and PNG formats to the minimum possible size.",
+    images: "/assets/hero.svg",
+  },
+};
 
 export default function RootLayout({
   children,
